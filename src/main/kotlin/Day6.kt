552b464414171f785e4c5f9720094f71faab9d69
tronglt <trongle1711@gmail.com>
2021-12-07 12:49:52 +0700

    Refactor

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,29 +1,19 @@
 fun main(args: Array<String>) {
     fun solvePuzzles(input: List<Int>, days: Int): Long {
-        val timers = mutableMapOf<Int, Long>(
-            0 to 0,
-            1 to 0,
-            2 to 0,
-            3 to 0,
-            4 to 0,
-            5 to 0,
-            6 to 0,
-            7 to 0,
-            8 to 0
-        ).let { timers ->
-            input.toMutableList().forEach { timers[it] = timers[it]!! + 1 }
+        val timers = Array(9) { 0L }.let { timers ->
+            input.toMutableList().forEach { timers[it] = timers[it] + 1 }
             timers
         }
 
         repeat(days) {
-            val news = timers[0]!!
+            val news = timers[0]
 
-            (1..8).forEach { timers[it - 1] = timers[it]!! }
+            (1..8).forEach { timers[it - 1] = timers[it] }
 
-            timers[6] = timers[6]!!.plus(news)
+            timers[6] = timers[6].plus(news)
             timers[8] = news
         }
-        return timers.values.sum()
+        return timers.sum()
     }
 
     val input = readEntireFile("src/main/resources/day_6.txt").split(",").map { it.toInt() }
